dd1a060fe71d4052cbdf665aa90eb311783e5299
Lansional <cbg1720@gmail.com>
2024-09-27 10:35:51 +0900

    delete unused file

--- terminal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TERMINAL_H__
-#define __TERMINAL_H__
-
-void gotoxy(const int, const int);
-void terminalClear(void);
-void terminalSize(const int x, const int y);
-
-#endif // __TERMINAL_H__
