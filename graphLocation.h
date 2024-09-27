73dccf7692bb08e250c4b3390286c88e11de01ec
Lansional <cbg1720@gmail.com>
2024-09-27 10:37:52 +0900

    delete wrong file

--- graphLocation.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GRAPHLOCATION_H__
-#define __GRAPHLOCATION_H__
-
-int selectTheXYData(const char*);								// 어떤 데이터로 x를 할 건지 선택
-void figurePointing(const char*, const int, const int);			// 점으로 표현하기
-
-#endif // !__GRAPHLOCATION_H__
